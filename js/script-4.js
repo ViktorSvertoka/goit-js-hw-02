5e33da74305b13c1e2708baf0c2b542d6656d04a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-01 23:21:29 +0200

    Feat(js): added script to task 4 - task done

--- js/script-4.js
+++ js/script-4.js
@@ -1,19 +1,55 @@
-const credits = 23580;
+// # Завдання 4
 
-const pricePerDroid = 3000;
+// Напиши функцію `formatString(string)` яка приймає рядок і форматує його якщо
+// необхідно.
 
-let quantity = prompt('Введіть кількість дроідив');
+// - Якщо довжина рядка не перевищує `40 символів`, функція повертає її в
+//   початковому   вигляді.
+// - Якщо довжина більше `40 символів`, то функція обрізає рядок до 40-ка символів
+//   і додає в кінець рядка три крапки `'...'`, після чого повертає   укорочену
+//   версію.
 
-let totalPrice = quantity * pricePerDroid;
+// ```js
+// const formatString = function (string) {
+//   // твій код
+// };
 
-if (quantity === null) {
-  console.log('Скасовано користувачем!');
-} else if (totalPrice > credits) {
-  console.log('Недостатньо коштів на рахунку!');
-} else {
-  console.log(
-    `Ви купили ${quantity} дроїдів, на рахунку залишилося ${
-      credits - totalPrice
-    } кредитів.`,
-  );
-}
+// /*
+//  * Виклич функції для перевірки працездатності твоєї реалізації.
+//  */
+// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// // повернеться оригінальний рядок
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// // повернеться форматований рядок
+
+// console.log(formatString("Curabitur ligula sapien."));
+// // повернеться оригінальний рядок
+
+// console.log(
+//   formatString(
+//     "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+//   )
+// );
+// // повернеться форматований рядок
+// ```
+
+const formatString = function (string) {
+  if (string.length >= 40) {
+    const over = string.slice(0, 39);
+    string = over + '...';
+  }
+  return string;
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+
+console.log(formatString('Curabitur ligula sapien.'));
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
