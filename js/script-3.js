783e127a95c3f85c75eb26c4d8d50e373d7f1e88
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-01 22:52:49 +0200

    Update script-3.js

--- js/script-3.js
+++ js/script-3.js
@@ -1,24 +1,3 @@
-// # Завдання 3
-
-// Напиши функцію `findLongestWord(string)`, яка приймає параметром довільний рядок
-// (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому
-// рядку.
-
-// ```js
-// const findLongestWord = function (string) {
-//   // твій код
-// };
-
-// /*
-//  * Виклич функції для перевірки працездатності твоєї реалізації.
-//  */
-// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
-
-// console.log(findLongestWord("Google do a roll")); // 'Google'
-
-// console.log(findLongestWord("May the force be with you")); // 'force'
-// ```
-
 const findLongestWord = function (string) {
   const array = string.split(' ');
   console.log(array);
